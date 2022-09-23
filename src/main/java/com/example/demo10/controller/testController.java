12f7ec0901a934a4be1f305977ab1331bb91bfbe
zhangqi <zhangqi8321@qq.com>
2022-09-23 15:29:36 +0800

    Initial commit

---
+++ src/main/java/com/example/demo10/controller/testController.java
@@ -0,0 +1,18 @@
+package com.example.demo10.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author zq
+ * @Date 2022-09-23
+ **/
+@RestController
+public class testController {
+
+
+    @GetMapping("/getTest")
+    public String test(){
+        return "hello,world";
+    }
+}
